1117d7338619325bfb4ba6fb100b5bca4cc09d6c
RafaelPetr <69591645+RafaelPetr@users.noreply.github.com>
2021-03-21 16:18:54 -0300

    Download de Arquivo ao ir para o formulário
    -Descartado a ideia do Trigger;
    -Download do arquivo é ativado pelo botão de formulário;

--- Assets/Scripts/Cenas/Formulario.cs
+++ Assets/Scripts/Cenas/Formulario.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Formulario : MonoBehaviour{
     public void AbrirFormulario() {
+        SistemaSalvamento.BaixarDado();
         Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSf2f-mnaMh10LtwX3cZGKRHDdwi6zp-sTsSIYojJjXG6kIn5w/viewform?usp=sf_link");
         Application.Quit();
     }
